46ed8a7814956e60d5149287cf99cf56d97f98e9
RRRAMOS <dsraquelramos@gmail.com>
2021-05-29 22:04:45 -0300

    interface para cadastro

---
+++ View/primeiroAcesso.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/fontawesome.min.css">
+    <title>Primeiro Acesso</title>
+</head>
+
+<body>
+    <div class="w3-col" style="width:11%;">
+        <i class="w3-xxlarge fa fa-envelope-o"></i>
+    </div>
+    <div class="w3-rest">
+        <input class="w3-input w3-border w3-round-large" name="txtEmail" type="text" placeholder="Email">
+    </div>
+    </div>
+    <div class="w3-row w3-section">
+        <div class="w3-col" style="width:11%">
+            <i class="w3-xxlarge fa fa-lock"></i>
+        </div>
+        <div class="w3-rest">
+            <input class="w3-input w3-border w3-round-large" name="txtSenha" type="password" placeholder="Senha">
+        </div>
+    </div>
+
+    <div class="w3-row w3-section">
+        <div class="w3-center" style="">
+            <button name="btnCadastrar" class="w3-button w3-block w3-margin w3-blue w3-cell w3-roundlarge" style="width: 90%;">Cadastrar</button>
+        </div>
+    </div>
+    </form>
+</body
