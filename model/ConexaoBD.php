31c7a2dfd955aaf0a5ca660b7792af6d8a659f6e
RRRAMOS <dsraquelramos@gmail.com>
2021-06-01 19:51:48 -0300

    correção sintaxe e senha

--- model/ConexaoBD.php
+++ model/ConexaoBD.php
@@ -1,16 +1,16 @@
 <?php
 Class ConexaoBD {
 
-private $serverName = "localhost";
-private $userName = "root";
-private $password = "";
-private $dbName = "bd_curriculos";
-}
+    private $servername = "localhost";
+    private $username = "root";
+    private $password = "12345678";
+    private $dbname = "bd_curriculos";
 
-public function conectar ()
-{
-    $conn = new mysql($this ->serverName, $this->userName, $this->passaword, $this->dbName);
-    return $conn;
+    public function conectar()
+    {
+        $conn = new mysqli($this ->servername, $this->username, $this->password, $this->dbname);
+        return $conn;
+    }
 }
 
 ?>
