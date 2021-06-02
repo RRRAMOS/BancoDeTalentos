cc6781543533843df5ea6b84ae9ce66646a13061
RRRAMOS <dsraquelramos@gmail.com>
2021-06-01 22:24:40 -0300

    revisão atributos

--- Controller/UsuarioController.php
+++ Controller/UsuarioController.php
@@ -1,24 +1,22 @@
 <?php
 if(!isset($_SESSION)) 
  { 
- session_start(); 
+    session_start(); 
  } 
 class UsuarioController{
  
- 
- public function inserir($nome, $cpf, $dataNascimento, $email, $senha) {
- require_once '../Model/Usuario.php';
- $usuario = new Usuario();
- $usuario->setNome($nome);
- $usuario->setcpf($cpf);
- $usuario->setDataNascimento($dataNascimento); 
- $usuario->setEmail($email); 
- $usuario->setSenha($senha); 
- //return $usuario->getNome();
- $r = $usuario->inserirBD();
- $_SESSION['Usuario'] = serialize($usuario);
- return $r; 
+    public function inserir($nome, $cpf, $email, $senha) {
+        require_once '../Model/Usuario.php';
+        $usuario = new Usuario();
+        $usuario->setNome($nome);
+        $usuario->setcpf($cpf);
+        $usuario->setEmail($email); 
+        $usuario->setSenha($senha); 
+        //return $usuario->getNome();
+        $r = $usuario->inserirBD();
+        $_SESSION['Usuario'] = serialize($usuario);
+        return $r; 
+    }
  }
-}
 ?>
 
